79e720b6011f060f0ca2f4f7ab7db68a2de17757
Aayush Bakre <29601578+WebHead2211@users.noreply.github.com>
2023-04-15 22:18:34 +0530

    Give functionality to decimal button
    Can add decimal point to the numbers.

--- index.js
+++ index.js
@@ -1,72 +1,100 @@
-let display = document.querySelector('p');
+let display = document.querySelector('span');
 const clearButton = document.querySelector('.btn-clear');
 const delButton = document.querySelector('.btn-del');
 const numButton = document.querySelectorAll('.btn-num');
 const opButton = document.querySelectorAll('.btn-op');
 const equalButton = document.querySelector('.btn-calc');
+const decimalButton = document.querySelector('.btn-float');
 
 let number = {
-    numOne: null,
-    numTwo: null,
+    numOne: 0,
+    numTwo: 0,
 };
 let operator = null;
 
 
+//NUM AND OP BUTTONS (TO SELECT EACH BUTTON)
+numButtonClick();
+opButtonClick();
 
 
+//EQUAL BUTTON
+equalButton.addEventListener('click', equalClick);
+
+//CLEAR AND DELETE BUTTON
 clearButton.addEventListener('click', clearScreen);
-
-delButton.addEventListener('click', () => {
-    display.textContent = removeCharAt(display.textContent);
-    if(number.numOne && operator && number.numTwo){
-        number.numTwo = removeNumber(number.numTwo);
-    } else if (operator){
-        operator = null; 
-    } else {
-        number.numOne = removeNumber(number.numOne);
-    }
-});
+delButton.addEventListener('click', delClick);
 
 
 
-function removeNumber(number){
-    return Math.floor(number / 10);
+//NUM AND OP BUTTON EVENT LISTENER
+function numButtonClick() {
+    numButton.forEach(button => {
+        button.addEventListener('click', numberClick);
+    });
 }
 
-function removeCharAt(string){
-    const strLength = string.length;
-    let tmpStr = string.split('');
-    tmpStr.splice(strLength - 1, 1);
-    return tmpStr.join(''); 
-}
-
-
-//Add numbers to display
-numButton.forEach(button => {
-    button.addEventListener('click', numberClick);
-});
-
-
-function numberClick(){
+function decimalButtonClick(){
     if(!operator){
-        if(number.numOne == null){
-            number.numOne = +this.textContent;
-        } else {
-            number.numOne = ((number.numOne * 10) + (this.textContent * 1));
+        if(!number.numOne.toString().includes('.')){
+            number.numOne = (number.numOne +'.');
+            display.textContent += '.';
         }
-        display.textContent = number.numOne;
     } else {
-        number.numTwo = ((number.numTwo * 10) + (this.textContent * 1));
-        display.textContent = `${number.numOne} ${operator} ${number.numTwo}`;
-    } console.log(number);
+        if(!number.numTwo.toString().includes('.')){
+            number.numTwo = (number.numTwo +'.');
+            display.textContent += '.';
+        }
+    }
 }
 
-opButton.forEach(button => {
-    button.addEventListener('click', function(){
-        if(!operator){
+function opButtonClick() {
+    opButton.forEach(button => {
+        button.addEventListener('click', opClick);
+    })
+}
+
+//DECIMAL BUTTON
+decimalButton.addEventListener('click', decimalButtonClick);
+
+//NUM CLICK FUNCTION
+function numberClick() {
+    if (limiter(430)) {
+        numButton.forEach(button => {
+            button.removeEventListener('click', numberClick);
+        });
+    } else {
+        numButtonClick();
+        if (!operator) {
+            if (number.numOne == null) {
+                number.numOne = +this.textContent;
+            } else {
+                // number.numOne = ((number.numOne * 10) + (this.textContent * 1));
+                number.numOne = +(number.numOne + this.textContent);
+            }
+            display.textContent = number.numOne;
+        } else {
+            number.numOne = +(number.numOne);
+            // number.numTwo = ((number.numTwo * 10) + (this.textContent * 1));
+            number.numTwo = +(number.numTwo + this.textContent);
+            display.textContent = `${number.numOne} ${operator} ${number.numTwo}`;      
+        }
+        console.log(number);
+    }
+}
+
+
+//OP CLICK FUNCTION
+function opClick() {
+    if (limiter(430) && !number.numTwo) {
+        opButton.forEach(button => {
+            button.removeEventListener('click', opClick);
+        });
+    } else {
+        if (!operator) {
             operator = this.textContent;
             display.textContent += ` ${operator}`;
-        } else if((number.numOne && number.numTwo && operator)){
+        } else if ((number.numOne && number.numTwo && operator)) {
             let answer = operate(number.numOne, operator, number.numTwo);
             number.numOne = answer;
             operator = this.textContent;
@@ -74,40 +102,86 @@ opButton.forEach(button => {
             display.textContent = `${number.numOne} ${operator}`;
         }
         console.log(operator);
-    });
-})
-
-equalButton.addEventListener('click', function(){
-    if (number.numOne && number.numTwo && operator){
-        let answer = operate(number.numOne, operator, number.numTwo);
-        display.textContent = answer;
-        number.numOne = answer;
-        number.numTwo = null;
-        operator = null;
-    } else if (number.numOne == 0 || number.numTwo == 0){
-        let answer = operate(number.numOne, operator, number.numTwo);
-        display.textContent = answer;
-        number.numOne = answer;
-        number.numTwo = null;
-        operator = null;
-    } 
-    else {
-        display.textContent = 'INVALID';
+        numButtonClick();
     }
-});
+}
 
 
-let stringLength = display.textContent.length;
-if (stringLength > 10){
-   display.textContent = stringLength;
+//EQUAL BUTTON CLICK FUNCTION
+function equalClick() {
+    if (number.numOne && number.numTwo && operator) {
+        let answer = operate(number.numOne, operator, number.numTwo);
+        display.textContent = answer;
+        number.numOne = answer;
+        number.numTwo = '';
+        operator = null;
+    } else if ((number.numOne == 0 || number.numTwo == 0) && operator) {
+        let answer = operate(number.numOne, operator, number.numTwo);
+        display.textContent = answer;
+        number.numOne = answer;
+        number.numTwo = '';
+        operator = null;
+    } else if (!operator && !number.numTwo) {
+        if (number.numOne == 0) {
+            display.textContent = '0';
+        }
+        display.textContent = `${number.numOne}`;
+    }
+    numButtonClick();
+}
+
+
+//DEL CLICK FUNCTION
+function delClick() {
+    if(!limiter()){
+        numButtonClick();
+        opButtonClick();
+    }
+    display.textContent = removeCharAt(display.textContent);
+    if (number.numOne && operator && number.numTwo) {
+        // number.numTwo = removeNumber(number.numTwo);
+        // number.numTwo = +(removeCharAt(number.numTwo.toString()));
+        number.numTwo = +(display.textContent.replace(`${number.numOne.toString()} ${operator}`, ''));
+    } else if (operator && !number.numTwo) {
+        display.textContent = removeCharAt(display.textContent);
+        operator = null;
+    } else if (!operator) {
+        if (number.numOne < 0 && number.numOne > -10) {
+            number.numOne = 0;
+        }
+        // number.numOne = removeNumber(number.numOne);
+        // number.numOne = +(removeCharAt(number.numOne.toString()))
+        number.numOne = +display.textContent;
+        operator = null;
+    }
+}
+
+function removeNumber(number) {
+    return Math.floor(number / 10);
+}
+
+function removeCharAt(string) {
+    const strLength = string.length;
+    let tmpStr = string.split('');
+    tmpStr.splice(strLength - 1, 1);
+    return tmpStr.join('');
+}
+
+//TO CLEAR SCREEN AND RESET TO DEFAULT
+function clearScreen() {
+    display.textContent = 0;
+    number.numOne = '';
+    number.numTwo = '';
+    operator = null;
+    numButtonClick();
+    opButtonClick();
 }
 
 
 
-
-//Calculation Functions
-function operate(num1, op, num2){
-    switch (op){
+//CALCULATION FUNCTION
+function operate(num1, op, num2) {
+    switch (op) {
         case '+':
             return add(num1, num2);
         case '-':
@@ -115,40 +189,55 @@ function operate(num1, op, num2){
         case '*':
             return multiply(num1, num2);
         case '/':
-            if(num2 == 0){
+            if (num2 == 0) {
                 window.alert('You cant divide by 0!');
                 clearScreen();
                 break;
             }
             return divide(num1, num2);
         case '%':
-            return modulus(num1, num2);   
+            return modulus(num1, num2);
     }
 }
 
-function add(a, b){
+
+//MATH FUNCTIONS
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a, b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+    return (a * b).toFixed(2).replace(/[.,]00$/, "");
 }
 
-function divide(a, b){
-    return a / b;
+function divide(a, b) {
+    return (a / b).toFixed(2).replace(/[.,]00$/, "");
 }
 
-function modulus(a, b){
-    return a % b;
+function modulus(a, b) {
+    return (a % b).toFixed(2).replace(/[.,]00$/, "");
 }
 
-function clearScreen(){
-    display.textContent = '';
-    number.numOne = null;
-    number.numTwo = null;
-    operator = null;
+function limiter(maxWidth){
+    return display.clientWidth > maxWidth;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
